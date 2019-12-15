e8fadfd42685889d447dbf7b4b4be3448f225353
Diego <dih.cvel@gmail.com>
2019-12-15 15:10:27 -0300

    coisas

---
+++ Lista 3/24.c
@@ -0,0 +1,13 @@
+// 24)Escreva um programa que receba três entradas, um valor, uma posição de bit e uma operação.
+// O programa deve executar a operação escolhida sobre o valor:
+// a) Escrever um bit a posição indicada.
+// b) Apagar um bit na posição indicada.
+// c) Ler um bit na posição indicada
+#include <stdio.h>
+
+int main () {
+    int a, b, c;
+    scanf ("%d%d%d", &a, &b, &c);
+    printf ("")
+    return 0;
+}
---
+++ Lista 4/1.c
@@ -0,0 +1,6 @@
+// 1) Implemente um programa para uma revendedora de carros que calcule o salário de cada vendedor. 
+// Considere que todos os carros têm o mesmo valor, e que a revendedora paga:
+// a) um salário fixo de dois salários mínimos a seus vendedores
+// b) uma comissão fixa de R$ 50,00 por carro que for vendido
+// c) caso o vendedor tenha vendido mais de 10 carros, uma comissão variável de 5% do valor total 
+// de suas vendas.

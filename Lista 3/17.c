26528b70fc49453bb9d1f703eb9a6463e7bbc011
Diego <dih.cvel@gmail.com>
2019-12-14 21:29:33 -0300

    Exercicio 17

---
+++ Lista 3/17.c
@@ -0,0 +1,11 @@
+// 17) Escreva um programa que receba duas entradas e imprima “1” caso um valor seja
+// divisível pelo outro e “0” caso contrário. Use apenas operadores aritméticos, lógicos ou
+// binários, não use IF.
+#include <stdio.h>
+
+int main () {
+    int a, b;
+    scanf ("%d%d", &a, &b);
+    ((a%b) == 0) ? printf ("1") : printf ("0");
+    return 0;
+}

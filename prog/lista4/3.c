ce5fa96c475552e4da60cc8f4eaf822814786f6d
srdiegoibs <dih.cvel@gmail.com>
2022-03-22 00:26:08 -0300

    up

--- prog/lista4/3.c
+++ prog/lista4/3.c
@@ -8,27 +8,23 @@
 #include <stdio.h>
 
 int main() {
-    int num = 0, cont = 0, sum = 0;
-    int vet[4] = {0, 0, 0, 0};
+    int num = 0, cont = 0, sum = 0; 
+    int aux = 0;
    
     scanf("%d", &num);
+    aux = num;
     
     if (num == 0) cont = 1;
     else
       while (num != 0) {
-	num /= 10;
-	vet[cont] = num%10;
-	cont++;
+    	sum += num%10;
+    	num /= 10;
+    	cont++;
       }
 
     if (cont > 4) printf ("ERROR!\n");
-    else
-      for (int i = 0; i < cont; i++) printf ("[%d]", vet[i]);
-
-
-
-
-    printf("did =%d\n", cont);
+    else 
+        printf("number: %d\nsum %d\n", aux, sum);
 
     return 0;
 }

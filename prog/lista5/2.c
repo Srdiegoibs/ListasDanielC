c00c062056e8280ca8abd3ed13615c25794caada
srdiegoibs <dih.cvel@gmail.com>
2022-04-18 22:03:17 -0300

    up

--- prog/lista5/2.c
+++ prog/lista5/2.c
@@ -4,39 +4,39 @@
 
 #define N 8
 
-int main () {
+int main()
+{
 	long long int **mat, i, j, k = 0;
-    long double s = 0;
+	long double s = 0;
+	mat = malloc(N * sizeof(long long int *));
 
-	mat = malloc (N * sizeof(long long int*));
+	for (i = 0; i < N; i++)
+		mat[i] = malloc(N * sizeof(long long int));
 
-	for (i=0; i<N; i++)
-		mat[i] = malloc (N * sizeof (long long int));
+	for (i = 0; i < N; i++)
+		for (j = 0; j < N; j++)
+		{
+			mat[i][j] = (pow(2.0, k));
+			s += mat[i][j];
+			printf("\ns = %0.Lf \n", s);
+			k++;
+		}
 
+	for (i = 0; i < N; i++)
+	{
+		for (j = 0; j < N; j++)
+		{
+			printf("[%lld]", mat[i][j]);
+		}
+		printf("\n");
+	}
 
-	for (i=0; i<N; i++) 
-        for (j=0; j<N; j++) {
-            mat[i][j] = (pow(2.0, k));
-            s += mat[i][j];
-            printf ("\ns = %0.Lf \n", s);
-            k++;
-        }
-    
+	printf("\n %0.Lf \n", s);
 
-	for (i=0; i<N; i++) {
-		for (j=0; j<N; j++) {
-		       printf("[%lld]", mat[i][j]);	
-        }
-        printf("\n");
-    }
+	for (i = 0; i < N; i++)
+		free(mat[i]);
 
-    printf ("\n %0.Lf \n", s);
-
-	for (i=0; i<N; i++)
-		free (mat[i]);
-
-	free (mat);
+	free(mat);
 
 	return 0;
-
 }

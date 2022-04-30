e5d65b34107feaeec2f708496495624d6e860c7c
Diego <Dih.cvel@gmail.com>
2022-04-30 18:20:47 -0300

    Update 16.c

--- prog/lista3/16.c
+++ prog/lista3/16.c
@@ -8,7 +8,7 @@ int main() {
     scanf ("%d", &a);    
     // AND binário. Copia um bit para o resultado
     // caso exista em ambos operandos.
-    // se o numero for  par toda vez que realizar a aperação
+    // se o numero for  par toda vez que realizar a operação
     // AND binario com 1 o resto vai ser falso, caso seja impar a 
     // operação AND com  vai ser verdadeira
     (a & 1) ? printf ("impar 0") : printf ("par 1");

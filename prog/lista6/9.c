4cf64608cbc01952a765cbfcb90b5d7cbf6d22d4
Diego <dih.cvel@gmail.com>
2022-10-15 00:58:23 -0300

    up

--- prog/lista6/9.c
+++ prog/lista6/9.c
@@ -6,9 +6,25 @@
 
 int main()
 {
+    int cont = 0, i = 0, j = 0;
     char *str = malloc(MAX_LENGTH);
-    str
-    char *subStr =  malloc(MAX_LENGTH)
+    scanf("%[^\n]s", str);
+    char *subStr =  malloc(MAX_LENGTH);
+    scanf("%s", subStr);
+
+    // printf("%s\n", str);
+    // printf("%s\n", subStr);
+
+    while (*str != '\0') {
+        while (subStr[0] == str[])
+        if (*str == 32 && *str != '\0') {
+            // printf ("> %c\n", *str);
+            printf ("> %d\n", cont);
+            cont++;
+        }
+        i++;
+        str++;
+    }
 
     return 0;
 }

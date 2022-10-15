d0d35a54f7100fd7a2db58c8b86c280b182872a3
Diego <dih.cvel@gmail.com>
2022-10-15 19:15:16 -0300

    vlw luis

--- prog/lista6/9.c
+++ prog/lista6/9.c
@@ -1,30 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 #define MAX_LENGTH 100
 
 
+int len (char *str);
+int cmp (const char *s1, const char *s2);
+
 int main()
 {
-    int cont = 0, i = 0, j = 0;
+    int cont = 1, size = 0, found = 0;
     char *str = malloc(MAX_LENGTH);
-    scanf("%[^\n]s", str);
     char *subStr =  malloc(MAX_LENGTH);
+    char *aux = malloc(0);
+
+    scanf("%[^\n]s", str);
     scanf("%s", subStr);
+    
+    int n = len(str);
+    for (int i = 0; i < n; i++) {
+        if (str[i] == ' ' || i == n-1) {
+            if (i == n-1) {
+                size++;
+                aux = (char *) realloc( aux, size*sizeof(char) );
+                aux[size-1] = str[i];        
+            }
 
-    // printf("%s\n", str);
-    // printf("%s\n", subStr);
+            if (cmp(aux, subStr) == 0) {
+                found = 1;
+                break;
+            }
 
-    while (*str != '\0') {
-        while (subStr[0] == str[])
-        if (*str == 32 && *str != '\0') {
-            // printf ("> %c\n", *str);
-            printf ("> %d\n", cont);
+            size = 0;
             cont++;
+            aux = (char *) realloc( aux, 0*sizeof(char) );
         }
-        i++;
-        str++;
+        else {
+            size++;
+            aux = (char *) realloc( aux, size*sizeof(char) );
+            aux[size-1] = str[i];
+        }
     }
 
+    if (!found) {
+        printf("não encontrado!\n");
+    }
+    else printf ("%d\n", cont);
+
     return 0;
+}
+
+int len (char *str) {
+    int n = 0;
+    while(*str != '\0') str++, n++;
+    return n;
+}
+
+int cmp (const char *s1, const char *s2) {
+    const unsigned char *us1 = (const unsigned char *) s1;
+    const unsigned char *us2 = (const unsigned char *) s2;
+
+    while (*us1 == *us2 && *us1 != '\0') us1++, us2++;
+
+    return (*us1 > *us2) - (*us1 < *us2);
 }

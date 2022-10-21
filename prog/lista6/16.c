26dff0669a4dc17f70d633608a22d80c248b5453
Diego <dih.cvel@gmail.com>
2022-10-21 12:07:09 -0300

    up

---
+++ prog/lista6/16.c
@@ -0,0 +1,3 @@
+// 16) Considerando que a função main recebe dois parâmetros “main (int args, char
+// *argv[])”, faça um programa que recebe diversas palavras como entrada pela linha de
+// comando e imprime qual é o maior dos parâmetros. Sugestão de entradas:

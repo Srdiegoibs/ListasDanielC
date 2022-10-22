9b9ac1a504614209383c5616ec7348d1aeb82f57
Diego <dih.cvel@gmail.com>
2022-10-22 15:43:09 -0300

    up

---
+++ prog/lista6/19.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    char **s;
+    char foo[] = "Ola mundo!";
+    **s = &foo;
+    printf("A string: %s\n", s);
+    // s[0] = foo;
+    // printf("O caractere s[0]: %s\n", s[0]);
+    return 0;
+}
